78ea2daf327d2b964a039d261a183308b96b5998
monkstone <mamba2928@yahoo.co.uk>
2017-11-05 16:44:04 +0000

    REMOVED superfluous coding of colors, but is there any point to lights?

--- fireflies.rb
+++ fireflies.rb
@@ -1,10 +1,7 @@
 # Fireflies
 load_library :control_panel
-COL_NAME = %w[sand rain green water black light].freeze
-COL_HEX = [0xF2E8C4, 0x98D9B6, 0x3EC9A7, 0x2B879E, 0x616668, 0xc9f202]
-PALETTE = COL_NAME.zip(COL_HEX).to_h
 
-attr_reader :panel, :hide, :mask_image, :flies, :spot_image
+attr_reader :panel, :hide, :mask_image, :flies, :spot_image, :back_image
 
 def settings
   size 1024, 480, P2D
@@ -23,8 +20,7 @@ def setup
     @panel = c
   end
   @hide = false
-  @spot_image = create_spot
-  @background = load_image(data_path('background.png'))
+  @back_image = load_image(data_path('background.png'))
   @mask_image = load_image(data_path('mask.png'))
   @spots = load_spots(mask_image, 4)
   reset!
@@ -39,22 +35,11 @@ def draw
     panel.show
     @hide = true
   end
-  image @background, 0, 0
-  draw_lights
+  image back_image, 0, 0
+  # draw_lights
   draw_flies
-end
-
-def draw_lights
-  lights = create_graphics width, height, P2D
-  lights.begin_draw
-  flies.each do |fly|
-    lights.push_matrix
-    lights.translate fly.pos.x, fly.pos.y
-    lights.image spot_image, 0, 0
-    lights.pop_matrix
-  end
-  lights.end_draw
-  mask_image.mask lights
+  load_pixels
+  mask_image.mask pixels
   image mask_image, 0, 0
 end
 
@@ -63,11 +48,11 @@ def draw_flies
   flies.each do |fly|
     # check if point reached
     if fly.pos.dist(fly.to_pos) < @target_radius
-      fly.to_pos = find_spot_near fly.pos, @spot_distance
+      fly.to_pos = near_spot fly.pos, @spot_distance
     end
     # set new rotation
     to_rotation = (fly.to_pos - fly.pos).heading
-    to_rotation = find_nearest_rotation(fly.rotation, to_rotation)
+    to_rotation = nearest_rotation(fly.rotation, to_rotation)
     # rotate to new direction
     if fly.rotation < to_rotation
       fly.rotation += rotation_max
@@ -97,7 +82,7 @@ Fly = Struct.new(:pos, :to_pos, :rotation, :positions)
 
 def create_fly
   spot = rand_spot
-  to_spot = find_spot_near spot, @spot_distance
+  to_spot = near_spot spot, @spot_distance
   rotation = rand * TWO_PI
   Fly.new(spot, to_spot, rotation, [])
 end
@@ -127,29 +112,16 @@ def rand_spot
   @spots.sample
 end
 
-def find_spot_near(position, distance)
+def near_spot(position, distance)
   spot = nil
   spot = rand_spot until spot && spot.dist(position) < distance
   spot
 end
 
-def find_nearest_rotation(from, to)
+def nearest_rotation(from, to)
   dif = (to - from) % TWO_PI
-  if dif != dif % PI
+  unless dif == dif % PI
     dif = dif < 0 ? dif + TWO_PI : dif - TWO_PI
   end
   from + dif
-end
-
-def create_spot
-  size = 30
-  glow = create_graphics size, size, P2D
-  glow.begin_draw
-  glow.no_stroke
-  glow.fill 255, 60
-  # glow.fill 255, 40
-  glow.ellipse size / 2, size / 2, size, size
-  glow.filter BLUR, 6
-  glow.end_draw
-  glow
 end
